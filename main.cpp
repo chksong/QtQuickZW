27bc49e1def2383b3deece6c6429d030e2573f0b
宋成魁 <songck@inhotech.com>
2018-11-07 15:04:27 +0800

    加入全局字体的设置

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QFont>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
+
+    //导入字体
+    QFont serifFont("Microsoft Sans Serif");
+    app.setFont(serifFont) ;
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
